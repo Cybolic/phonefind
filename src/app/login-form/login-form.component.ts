77050ab5f4c0d2ea473a65471f86de22c84b6d8c
Christian Dannie Storgaard <cds@cybolic.me>
2023-01-09 22:59:35 +0200

    Update project name, styling and add login component

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent {
+
+  // items = this.cartService.getItems();
+
+  loginForm = this.formBuilder.group({
+    phone: '',
+  });
+
+  constructor(
+    // private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    // this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.loginForm.value);
+    this.loginForm.reset();
+  }
+}
