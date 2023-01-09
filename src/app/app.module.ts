fde1f9fae8d9bc576a1dd0c184ccd771aec6f627
Christian Dannie Storgaard <cds@cybolic.me>
2023-01-09 23:09:47 +0200

    Added eslint and prettier (using supplied config)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { ReactiveFormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { LoginFormComponent } from './login-form/login-form.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-  ],
+  declarations: [AppComponent, LoginFormComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core'
+import { FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
-
   // items = this.cartService.getItems();
 
   loginForm = this.formBuilder.group({
     phone: '',
-  });
+  })
 
   constructor(
     // private cartService: CartService,
@@ -22,7 +21,7 @@ export class LoginFormComponent {
   onSubmit(): void {
     // Process checkout data here
     // this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.loginForm.value);
-    this.loginForm.reset();
+    console.warn('Your order has been submitted', this.loginForm.value)
+    this.loginForm.reset()
   }
 }
